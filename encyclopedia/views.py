a5a1acde020d4f375692291361ffa2bcc59c154d
Spoonbadger <craig.adam.morley@gmail.com>
2024-05-22 15:39:56 -0700

    search function complete

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -26,4 +26,32 @@ def markdown_to_html(title):
         return markdowner.convert(md_file)
 
     
-def search(request, )
+def search(request):
+    if request.method == "POST":
+        # Create a list of all entries
+        entries = util.list_entries()
+        # Get user input from the form
+        user_search = request.POST["q"].lower()
+        user_search = user_search.lower()
+        # Initialize list of possible matches
+        matches = []
+        # See if user_search matches an entry
+        for entry in entries:
+            if user_search == entry.lower():
+                return render(request, "encyclopedia/entry.html", {
+                    "title": user_search,
+                    "content": markdown_to_html(user_search)
+                })
+        # See if user_search is in an entry
+            elif user_search in entry.lower():
+                matches.append(entry)
+        if not matches:
+            return render(request, "encyclopedia/index.html", {
+                "entries": entries,
+            })
+        else:
+            return render(request, "encyclopedia/search.html", {
+            "matches": matches
+        })
+    
+
